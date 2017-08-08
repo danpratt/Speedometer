c726e177d6e91de9c3768ad31b5cf52712c2a569
Daniel Pratt <daniel@blaumagier.com>
2017-08-08 10:17:47 -0700

    feat: setup storyboard
    - added items
    - setup constraints

---
+++ Speedometer/UIGroup/UIBorderedButton.swift
@@ -0,0 +1,35 @@
+//
+//  UIBorderedButton.swift
+//  Speedometer
+//
+//  Created by Daniel Pratt on 8/8/17.
+//  Copyright © 2017 Daniel Pratt. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class UIBorderedButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+        
+    }
+
+    func commonInit() {
+        layer.cornerRadius = 5
+        layer.masksToBounds = true
+        layer.borderWidth = 1
+        layer.borderColor = titleLabel?.textColor.cgColor
+    }
+
+    override func prepareForInterfaceBuilder() {
+        commonInit()
+    }
+}
--- Speedometer/ViewController.swift
+++ Speedometer/ViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet var speedLabels: [UILabel]!
+    @IBOutlet weak var speedViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet var colorableViews: [UIView]!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
